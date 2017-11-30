bd948040729ebe66cb782c4b5dc412037901d459
Peter <lawsonpd@gmail.com>
2017-11-30 13:36:25 -0600

    Ignore settings.json. Move wordnik key to settings.json.

--- client/main.js
+++ client/main.js
@@ -5,8 +5,10 @@
 
 import { HTTP } from 'meteor/http';
 
+const wordnik_key = process.env.WORDNIK_KEY;
+
 function requestWord () {
-  HTTP.get("http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=idiom&minCorpusCount=1000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=88151505619400fe187d61357090727518edebec080dfdb55",
+  HTTP.get("http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=idiom&minCorpusCount=1000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=" + wordnik_key,
     function (error, response) {
       if (error) {
         console.log(error);
